f5145a8dd0cb0d6fb8d2663766fdaf78f17fa766
BeomYoonHeo <gqd0512@naver.com>
2022-10-04 18:53:57 +0900

    웹소켓 테스트 완료

---
+++ src/main/java/site/metacoding/websocket/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package site.metacoding.websocket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import site.metacoding.websocket.websocket.Greeting;
+import site.metacoding.websocket.websocket.HelloMessage;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
